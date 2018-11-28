9691642e2bc109a57c53e12ca42c8ca4a3bf08a2
whekin <stanislavkalishin@gmail.com>
2018-11-28 22:36:04 +0300

    Initial commit

---
+++ main.pas
@@ -0,0 +1,40 @@
+﻿program color; 
+const
+    pencilCount = 5;
+    n = 100;
+var
+    box: array [0..pencilCount - 1] of integer;
+    firstPencilIndex, secondPencilIndex, i: integer;
+    eventNumber: integer;
+
+function doExperiment(pencilCount: integer): array of integer;
+begin
+    
+end;
+
+begin 
+    box[0] := 0; 
+    box[1] := 0; 
+    box[2] := 0; 
+    box[3] := 1; 
+    box[4] := 1;
+    
+    for i := 0 to n - 1 do
+    begin
+        firstPencilIndex := Random(pencilCount); 
+        secondPencilIndex := Random(pencilCount);
+        
+        while firstPencilIndex = secondPencilIndex do
+            secondPencilIndex := Random(pencilCount);
+        
+        if box[firstPencilIndex] = box[secondPencilIndex] then
+            if box[firstPencilIndex] = 0 then
+                eventNumber := 1
+            else
+                eventNumber := 3
+        else
+            eventNumber := 2;
+        writeln(eventNumber);
+            
+    end;
+end.
